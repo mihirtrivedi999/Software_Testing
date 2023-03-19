f0bb3ea1da2717d38e411c21c45ab41a2a0340b2
mihirtrivedi999 <117349073+mihirtrivedi999@users.noreply.github.com>
2023-03-19 23:05:01 +0530

    Add files via upload

---
+++ automation/simpleinterest.java
@@ -0,0 +1,14 @@
+package task;
+
+public class simpleinterest {
+	public static void main(String[] arg)
+	{
+		double p=15000;
+		double r=10;
+		double n=2;
+		
+		double simpleinterest=p*r*n/100;
+		System.out.println(simpleinterest);
+		
+	}
+}

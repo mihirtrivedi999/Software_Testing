34f49682bfd9d872d18a713889b849d298ba8691
mihirtrivedi999 <117349073+mihirtrivedi999@users.noreply.github.com>
2023-05-11 15:33:18 +0530

    Add files via upload

---
+++ Nestedifex.java
@@ -0,0 +1,27 @@
+
+public class Nestedifex {
+	public static void main(String[] args) {
+		
+		int mark=80;
+		
+		if(mark>=75)
+			
+		{
+			System.out.println("Distinction");
+			
+		}
+				int mark1=61;
+				if(mark1>=65)
+				{
+					System.out.println("Firstclass");
+					
+				}
+		
+				else
+				{
+					
+					System.out.println("Fail");
+					
+				}
+	}
+}
